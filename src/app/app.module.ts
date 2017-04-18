46a33619083ba44de64933cb8c3c8ead2f42c3c9
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-04-18 20:29:17 +0530

    salary transfer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { Storage } from '@ionic/storage';
+import { HttpModule } from '@angular/http';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+import { IonicStorageModule } from '@ionic/storage';
+
+// Ionic Native
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { MyApp } from './app.component';
 
 // Start Pages [Logged Out]
 import { LoginPage } from '../pages/start-pages/login/login';
+import { SalaryPage } from '../pages/logged-in/account/salary/salary'; 
+
 // Pages when logged in
 import { NavigationPage } from '../pages/logged-in/navigation/navigation';
 import { HomePage } from '../pages/logged-in/home/home';
 
 // Providers / Services
+import { AuthHttpService } from '../providers/logged-in/authhttp.service';
 import { AuthService } from '../providers/auth.service';
 import { ConfigService } from '../providers/config.service';
+import { AccountService } from '../providers/logged-in/account.service';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': 'dfb10dee'
+  }
+};
 
 @NgModule({
   declarations: [
@@ -21,7 +38,8 @@ import { ConfigService } from '../providers/config.service';
     LoginPage,
     // Logged In
     NavigationPage,
-    HomePage
+    HomePage,
+    SalaryPage
   ],
   entryComponents: [
     MyApp,
@@ -29,16 +47,26 @@ import { ConfigService } from '../providers/config.service';
     LoginPage,
     // Logged In
     NavigationPage,
-    HomePage
+    HomePage,
+    SalaryPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings),
+    IonicStorageModule.forRoot()
   ],
   providers: [
+      // Ionic Native 
+      StatusBar,
+      SplashScreen,
       {provide: ErrorHandler, useClass: IonicErrorHandler},
-      Storage, // Ionic Storage
+      // Custom
       AuthService, // Handles all Authorization
-      ConfigService // Handles Environment-specific Variables
+      ConfigService, // Handles Environment-specific Variables
+      AuthHttpService,
+      AccountService
   ],
   bootstrap: [IonicApp]
 })
