f6c876921490e7e223a2101d890637f392f8a51e
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-05-19 18:07:43 +0530

    no internet

--- src/providers/config.service.ts
+++ src/providers/config.service.ts
@@ -46,10 +46,9 @@ export class ConfigService {
    * @param {string} [platform]
    */
   initProdEnvironment(platform?: string){
-    //this.apiBaseUrl = "http://payroll-candidate.dev.studenthub.co/v1";
+    this.apiBaseUrl = "http://payroll-candidate.dev.studenthub.co/v1";
     //this.apiBaseUrl = "http://localhost/payroll/candidate/web/v1";
-    
-    this.apiBaseUrl = "https://payroll-candidate.studenthub.co/v1";
+    //this.apiBaseUrl = "https://payroll-candidate.studenthub.co/v1";
     this.setupDeviceSpecificConfigs();
   }
 
