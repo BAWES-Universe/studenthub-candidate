65d39d606d44f92fa3062743e17b7b750b11de6c
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-07-17 13:32:55 +0530

    [Finishes #173832353] sentry error removed

--- src/app/providers/logged-in/account.service.ts
+++ src/app/providers/logged-in/account.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class AccountService {
 
-  private _accountEndpoint: string = "/account";
+  private _accountEndpoint = '/account';
 
   constructor(private _authhttp: AuthHttpService) { }
 
@@ -17,21 +17,21 @@ export class AccountService {
    * @returns {Observable<any>}
    */
   listSalary(page: number): Observable<any> {
-    let url = this._accountEndpoint + '/salary?page=' + page;
+    const url = this._accountEndpoint + '/salary?page=' + page;
     return this._authhttp.get(url, true);
   }
-  
+
   /**
    * Create
    * @param {oldPassword} string
    * @param {newPassword} string
    * @returns {Observable<any>}
    */
-  changePassword(oldPassword:string,newPassword:string): Observable<any>{
-    let postUrl = `${this._accountEndpoint}`+ '/change-password';
-    let params = {
-      "old_password": oldPassword,
-      "new_password": newPassword,
+  changePassword(oldPassword: string, newPassword: string): Observable<any>{
+    const postUrl = `${this._accountEndpoint}` + '/change-password';
+    const params = {
+      old_password: oldPassword,
+      new_password: newPassword,
     };
     return this._authhttp.post(postUrl, params);
   }
