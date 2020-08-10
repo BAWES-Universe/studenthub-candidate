f0a74c95d5a0b6f5197eaca5682c5bd0044416d7
Krushn <kathrechakrushn@gmail.com>
2020-08-10 15:22:08 +0530

    profile tabs updated

--- src/app/pages/logged-in/tabs/tabs.page.ts
+++ src/app/pages/logged-in/tabs/tabs.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform, PopoverController } from '@ionic/angular';
+//services
 import { TranslateLabelService } from 'src/app/providers/translate-label.service';
-import { Platform } from '@ionic/angular';
+//pages
+import { OptionPage } from '../option/option.page';
+
 
 @Component({
   selector: 'app-tabs',
@@ -11,10 +15,22 @@ export class TabsPage implements OnInit {
 
   constructor(
     public platform: Platform,
+    public popoverCtrl: PopoverController,
     public _translateService: TranslateLabelService
   ) { }
 
   ngOnInit() {
   }
 
+  /**
+   * Display Popover with Additional Actions (Change Password and Logout)
+   * @param e
+   */
+  async openPopover(e) {
+    const popover = await this.popoverCtrl.create({
+      component: OptionPage,
+      event: e
+    });
+    popover.present();
+  }
 }
