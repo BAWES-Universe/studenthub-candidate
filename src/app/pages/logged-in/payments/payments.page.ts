e9b00431bd5c6f047b337c71ee10c9cb196eefee
Krushn <kathrechakrushn@gmail.com>
2020-08-18 21:21:34 +0530

    thumb field removed + candidate api refactored to view store company by expand field

--- src/app/pages/logged-in/payments/payments.page.ts
+++ src/app/pages/logged-in/payments/payments.page.ts
@@ -157,6 +157,5 @@ export class PaymentsPage implements OnInit {
    */
   loadLogo($event, candidate) {
     candidate.candidate_personal_photo = null;
-    return candidate.candidate_personal_photo_thumb = null;
   }
 }
