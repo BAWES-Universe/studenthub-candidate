5033001a13207e5ef5bf3e4f4db9ac51d0ffe307
Krushn Kathrecha <kathrechakrushn@gmail.com>
2021-02-24 20:52:36 +0530

    model for request

---
+++ src/app/models/request.ts
@@ -0,0 +1,34 @@
+import { Company } from './company';
+
+
+export class Request {
+    request_uuid: string;
+    company_id: number;
+    contact_uuid: string;
+    request_created_by: number;
+    request_updated_by: number;
+    request_position_type: number;
+    request_position_title: string;
+    request_job_description: string;
+    request_compensation: string;
+    request_number_of_employees: number;
+    request_location: string;
+    request_additional_info: string;
+    request_status: any;
+    // request_status: Status;
+    request_feedback: string;
+    request_created_datetime: string;
+    request_updated_datetime: string;
+    /*requestCreatedBy: Staff;
+    requestUpdatedBy: Staff;
+    contact: Contact;
+    lastActivity: Note;*/
+    company: Company;
+}
+/*
+enum Status {
+    pending,
+    started,
+    delivered,
+    cancelled
+}*/
