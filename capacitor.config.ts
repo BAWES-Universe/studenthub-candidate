646a5cde9b823ef3eddf4a66515b102ed911b4e5
Anil Kumar <anilkumar.dhiman1@gmail.com>
2022-12-23 14:28:27 +0530

    built issue fixed

---
+++ capacitor.config.ts
@@ -0,0 +1,25 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.studenthub.candidate',
+  appName: 'SH Candidate',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    "SplashScreen": {
+      "launchAutoHide": false,
+      "showSpinner": false,
+      "launchShowDuration": 30000,
+      "backgroundColor": "#ffffffff",
+      "androidSplashResourceName": "splash",
+      "androidScaleType": "CENTER_CROP",
+      "androidSpinnerStyle": "large",
+      "iosSpinnerStyle": "small",
+      "spinnerColor": "#f24459",
+      "splashFullScreen": true,
+      "splashImmersive": true
+    }
+  }
+};
+
+export default config;
